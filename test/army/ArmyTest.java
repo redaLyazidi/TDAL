25b5e2331381e14b229bc55626420c5c0e6718ea
redaLyazidi <lyazidiredaqqch@hotmail.fr>
2012-11-08 01:27:17 +0100

    changement dans le code du prof, sa fonction heal s'appelle désormais infuse_life et la nouvelle "heal" quant à elle ne ressucite pas

---
+++ test/army/ArmyTest.java
@@ -0,0 +1,64 @@
+package army;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ArmyTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetHealthPoints() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAlive() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHeal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testParry() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStrike() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddEquipment() {
+		fail("Not yet implemented");
+	}
+
+}
